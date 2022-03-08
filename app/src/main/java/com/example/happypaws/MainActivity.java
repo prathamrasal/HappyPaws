1112c2d9c45e11b46848074641b61baad54a1f82
prathamrasal <prathamrasal6@gmail.com>
2022-03-08 19:16:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/happypaws/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.happypaws;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
